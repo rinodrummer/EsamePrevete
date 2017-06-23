8929bf63e653df26cd90a7e16dc43b92927e7c71
RinoDrummer <gennarolandolfi@codedwork.it>
2017-06-23 19:17:19 +0200

    Corrected the function to be in accordance with the task.

--- Ex. 3/Ex3.c
+++ Ex. 3/Ex3.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+enum MODE {WITH_RIP, NO_RIP};
+
+#define DEFAULT_MODE 0
+
 struct Lista {
     int x;
     int y;
@@ -8,19 +12,41 @@ struct Lista {
 };
 
 struct Lista *creaNodo(int x, int y);
-struct Lista *popolaLista(struct Lista *head, int n[], int c, int l, const int end);
+struct Lista *creaCoppieConRipetizioni(int n[], int c, int l, const int end);
+struct Lista *creaCoppieSenzaRipetizioni(int A[], int n);
 
 void stampaNodo(struct Lista *nodo);
-void stampaLista(struct Lista *head, int l);
+void stampaLista(struct Lista *head, int l, enum MODE m);
 
 int main(void) {
     struct Lista *l = NULL;
+
     int val[4] = {2, 8, 6, 10};
     int n = 4;
 
-    l = popolaLista(l, val, 0, 0, n);
+    unsigned short int c = 0;
 
-    stampaLista(l, n);
+    do {
+        printf("0. Crea coppie con ripetizioni;\n");
+        printf("1. Crea coppie senza ripetizioni;\n");
+
+        printf("\nScegli una modalita' di visualizzazione: ");
+        scanf("%hu", &c);
+        printf("\n");
+
+        if (c == 0) {
+            l = creaCoppieConRipetizioni(val, 0, 0, n);
+        }
+        else if (c == 1) {
+            l = creaCoppieSenzaRipetizioni(val, n);
+        }
+        else {
+            printf("Indicare una scelta valida.\n\n");
+        }
+    }
+    while(c > 1);
+
+    stampaLista(l, n, (enum MODE) c);
 
     return 0;
 }
@@ -35,7 +61,9 @@ struct Lista *creaNodo(int x, int y) {
     return nodo;
 }
 
-struct Lista *popolaLista(struct Lista *head, int n[], int c, int l, const int end) {
+struct Lista *creaCoppieConRipetizioni(int n[], int c, int l, const int end) {
+    struct Lista *head = NULL;
+
     if (head == NULL && (c < end && l < end)) {
         head = creaNodo(n[c], n[l++]);
 
@@ -44,19 +72,46 @@ struct Lista *popolaLista(struct Lista *head, int n[], int c, int l, const int e
             l = 0;
         }
 
-        head->next = popolaLista(head->next, n, c, l, end);
+        head->next = creaCoppieConRipetizioni(n, c, l, end);
+    }
+
+    return head;
+}
+
+struct Lista *creaCoppieSenzaRipetizioni(int A[], int n) {
+    struct Lista *head = NULL;
+
+    if (n == 0) {
+        return NULL;
+    }
+    else {
+        int i = 0;
+
+        head = creaNodo(A[0], A[i++]);
+
+        struct Lista *prev = head;
+        struct Lista *tmp = NULL;
+
+        for (; i < n; i++) {
+            tmp = creaNodo(A[0], A[i]);
+
+            prev->next = tmp;
+            prev = prev->next;
+        }
+
+        prev->next = creaCoppieSenzaRipetizioni(&A[1], --n);
     }
 
     return head;
 }
 
 void stampaNodo(struct Lista *nodo) {
-    printf("\t\t(%d, %d);\n", nodo->x, nodo->y);
+    printf("\t(%d, %d);\n", nodo->x, nodo->y);
 
     return;
 }
 
-void stampaLista(struct Lista *head, int l) {
+void stampaLista(struct Lista *head, int l, enum MODE m) {
     int n = 0;
 
     printf("\n");
@@ -65,6 +120,10 @@ void stampaLista(struct Lista *head, int l) {
         n++;
 
         if (n == l) {
+            if (m == NO_RIP) {
+                l--;
+            }
+
             printf("\n");
             n = 0;
         }
